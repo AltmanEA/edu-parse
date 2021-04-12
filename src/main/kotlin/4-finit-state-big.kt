809d9ea13043573afd02049c0e13709e3156a45e
Евгений Альтман <altmanea@gmail.com>
2021-04-12 11:03:44 +0600

    finite state done

--- src/main/kotlin/4-finit-state-big.kt
+++ src/main/kotlin/4-finit-state-big.kt
@@ -12,47 +12,84 @@ private val dataset: State = State {
 private val series: State = State {
     when (it) {
         XMLStreamReader.START_ELEMENT ->
-            if (reader.localName == "SeriesKey")
+            if (reader.localName == "SeriesKey") {
+                rightValues = 0
                 current = checkSeries
-        XMLStreamReader.END_ELEMENT ->
-            if (reader.localName == "Series")
-                current = dataset
+            }
     }
 }
 
 private val checkSeries: State = State {
     when (it) {
+        XMLStreamReader.START_ELEMENT ->
+            if (reader.localName == "Value") {
+                if ((reader.getAttributeValue(0) == "s_OKATO") and
+                    (reader.getAttributeValue(1) == "643")
+                ) rightValues++
+                if ((reader.getAttributeValue(0) == "s_OKVED2") and
+                    (reader.getAttributeValue(1) == "62")
+                ) rightValues++
+                if (rightValues == 2) {
+                    point = Point()
+                    current = rightSeries
+                }
+            }
+        XMLStreamReader.END_ELEMENT ->
+            if (reader.localName == "SeriesKey")
+                current = dataset
     }
 }
 
+private val rightSeries: State = State {
+    when (it) {
+        XMLStreamReader.START_ELEMENT -> {
+            if (reader.localName == "Attributes")
+                current = getAttributes
+            if (reader.localName == "Obs")
+                current = getObs
+        }
+        XMLStreamReader.END_ELEMENT ->
+            if (reader.localName == "Series"){
+                if(point.period!="")
+                    result.add(point)
+                current = dataset
+            }
+    }
+}
 
 private val getAttributes: State = State {
     when (it) {
-    }
-}
-
-private val getPeriod: State = State {
-    when (it) {
+        XMLStreamReader.START_ELEMENT ->
+            if (reader.localName == "Value")
+                if (reader.getAttributeValue(0) == "PERIOD") {
+                    val period = reader.getAttributeValue(1)
+                    if (!period.contains("-"))
+                        point.period = period
+                    current = rightSeries
+                }
     }
 }
 
 private val getObs: State = State {
     when (it) {
-    }
-}
-
-private val getTine: State = State {
-    when (it) {
-    }
-}
-
-private val getValue: State = State {
-    when (it) {
+        XMLStreamReader.START_ELEMENT ->
+            when (reader.localName) {
+                "Time" ->
+                    point.year = reader.elementText.toInt()
+                "ObsValue" ->
+                    point.value = reader.getAttributeValue(0)
+                        .replace(",", ".").toFloat()
+            }
+        XMLStreamReader.END_ELEMENT ->
+            if (reader.localName == "ObsValue")
+                current = rightSeries
     }
 }
 
 private val result = ArrayList<Point>()
 private var current: State = dataset
+private lateinit var point:Point
+private var rightValues = 0
 
 fun main() {
     while (reader.hasNext())
