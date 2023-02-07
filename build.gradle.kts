496138be4404114d24b62c0c6f00a875faee3338
AltmanEvgenii <AltmanEA@gmail.com>
2023-02-07 20:28:01 +0600

    upgrade kotlin 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.8.0"
     application
 }
 
@@ -12,12 +12,15 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion = "5.5.5"
+
 dependencies {
-    testImplementation(kotlin("test-junit"))
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
-tasks.test {
-    useJUnit()
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile>() {
